426c7cdc652911098152dee69df8b73b6f545bd7
Tim Buser <tim.buser@youengineering.ch>
2024-11-26 09:27:09 +0100

    Add content

---
+++ pages/_meta.tsx
@@ -0,0 +1,19 @@
+export default {
+  "*": {
+    type: "page",
+  },
+  index: {
+    title: "Übersicht",
+    type: "page",
+    theme: {
+      layout: "full",
+    },
+  },
+  SAD: {
+    index: {
+      title: "Software Architektur Dokumentation",
+    },
+    title: "Software Architektur Dokumentation",
+    type: "page",
+  },
+};
