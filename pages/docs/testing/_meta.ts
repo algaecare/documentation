eadcdd31d021e68a015aa7299a06e24f77ce0a80
ben.boehler <ben.boehler@students.fhnw.ch>
2024-11-28 10:49:14 +0100

    sort layout

---
+++ pages/docs/testing/_meta.ts
@@ -0,0 +1,10 @@
+export default {
+    Testplan: "Testplan",
+    smoketests: "1.0 Smoke Tests",
+    funktionstests: "2.0 Funktionstests",
+    integrationstests: "3.0 Integrationstests",
+    usabilitytests: "4.0 Usability-Tests",
+    abschließendeSystemtests: "5.0 Abschließende Systemtests",
+    case_00_template: "Case Template",
+  };
+  
