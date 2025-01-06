22beb5265f84b05c5f841df5fd195739724438dd
Tim Buser <tim.buser@youengineering.ch>
2025-01-06 23:35:05 +0100

    add docu-content

--- pages/docu/_meta.ts
+++ pages/docu/_meta.ts
@@ -1,3 +1,15 @@
 export default {
   einfuehrung: "Einführung",
+  "---1": {
+    type: "separator",
+    title: "Gitlab",
+  },
+  "---2": {
+    type: "separator",
+    title: "Dokumentation",
+  },
+  "---3": {
+    type: "separator",
+    title: "Raspberry Pi",
+  },
 };
