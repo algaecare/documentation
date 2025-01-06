36f4b1c8c395c21c7289983e39c8a2c0d6660088
Tim Buser <tim.buser@youengineering.ch>
2025-01-06 23:37:40 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,25 @@
-import nextra from 'nextra'
- 
+import nextra from "nextra";
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: {
-    unoptimized: true // mandatory, otherwise won't export
+    unoptimized: true, // mandatory, otherwise won't export
   },
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: "build"
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias["obsidian"] = false;
+      config.resolve.alias["@codemirror/state"] = false;
+      config.resolve.alias["@codemirror/view"] = false;
+    }
+    return config;
+  },
+};
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx'
-})
- 
-export default withNextra(nextConfig)
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+export default withNextra(nextConfig);
